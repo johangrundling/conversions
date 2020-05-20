c42e9dc74ab1545b9d1507ea2b1a25c7408ec940
Johan Grundling <johan.grundling@blackswan.com>
2020-05-20 20:54:13 +0200

    Fixed resource path

--- src/main/java/org/conversions/ConversionResource.java
+++ src/main/java/org/conversions/ConversionResource.java
@@ -5,7 +5,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/conversion")
+@Path("/conversions")
 public class ConversionResource {
 
     @GET
