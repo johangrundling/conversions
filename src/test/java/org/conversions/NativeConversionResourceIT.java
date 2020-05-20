ef11afda26022e95937fd00f277b36a80eb3b3ac
Johan Grundling <johan.grundling@blackswan.com>
2020-05-20 20:49:55 +0200

    Generated project files using command
    mvn io.quarkus:quarkus-maven-plugin:1.4.2.Final:create \
        -DprojectGroupId=org.conversions \
        -DprojectArtifactId=conversions \
        -DclassName="org.conversions.ConversionResource" \
        -Dpath="/conversion" \
        -Dextensions="resteasy-jackson"

---
+++ src/test/java/org/conversions/NativeConversionResourceIT.java
@@ -0,0 +1,9 @@
+package org.conversions;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeConversionResourceIT extends ConversionResourceTest {
+
+    // Execute the same tests but in native mode.
+}
